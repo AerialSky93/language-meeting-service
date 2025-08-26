7e0c6e316c4099823896cbad0f6b5e9b20c8b885
Alex Smith <rdutta23@yahoo.com>
2025-08-25 22:30:47 -0700

    add passport items

---
+++ src/service-auth/passport.ts
@@ -0,0 +1,33 @@
+const User = require('../models/index')['User'];
+
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+module.exports = function (passport) {
+  passport.serializeUser(function (user, done) {
+    done(null, user);
+  });
+
+  passport.deserializeUser(function (user, done) {
+    done(null, user);
+  });
+
+  passport.use(
+    new GoogleStrategy(
+      {
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        callbackURL: process.env.GOOGLE_APP_CALLBACK_URL,
+      },
+      async function (accessToken, refreshToken, profile, cb) {
+        const [user, status] = await User.findOrCreate({
+          where: {
+            social_user_id: profile.id,
+            name: profile.displayName,
+            registration_type: 'google',
+          },
+        });
+        cb(null, user);
+      },
+    ),
+  );
+};
