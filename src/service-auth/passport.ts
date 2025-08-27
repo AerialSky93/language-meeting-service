bb85dd68557ca3e1eb2712850cc4e823fe359269
Alex Smith <rdutta23@yahoo.com>
2025-08-26 22:59:13 -0700

    google oauth

--- src/service-auth/passport.ts
+++ src/service-auth/passport.ts
@@ -1,33 +1,46 @@
-const User = require('../service-auth/user');
+import { PassportStatic } from 'passport';
+import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
+import type { Profile } from 'passport-google-oauth20';
+import { UserAccountRepository } from '../repository/user-account-repository';
+import { Injectable } from '@nestjs/common';
 
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
+@Injectable()
+export class PassportService {
+  constructor(private readonly userAccountRepository: UserAccountRepository) {}
 
-module.exports = function (passport) {
-  passport.serializeUser(function (user, done) {
-    done(null, user);
-  });
+  initialize(passport: PassportStatic) {
+    passport.serializeUser((user: any, done: any) => {
+      done(null, user);
+    });
 
-  passport.deserializeUser(function (user, done) {
-    done(null, user);
-  });
+    passport.deserializeUser((user: any, done: any) => {
+      done(null, user);
+    });
 
-  passport.use(
-    new GoogleStrategy(
-      {
-        clientID: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: process.env.GOOGLE_APP_CALLBACK_URL,
-      },
-      async function (accessToken, refreshToken, profile, done) {
-        const [user, status] = await User.findOrCreate({
-          where: {
-            social_user_account_id: profile.id,
-            name: profile.displayName,
-            registration_type: 'google',
-          },
-        });
-        done(null, user);
-      },
-    ),
-  );
-};
+    passport.use(
+      new GoogleStrategy(
+        {
+          clientID: process.env.GOOGLE_CLIENT_ID!,
+          clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+          callbackURL: process.env.GOOGLE_APP_CALLBACK_URL!,
+        },
+        async (
+          accessToken: string,
+          refreshToken: string,
+          profile: Profile,
+          done: any,
+        ) => {
+          const user = await this.userAccountRepository.getOrCreateUserAccount(
+            profile.id,
+            profile.displayName,
+            'google',
+            profile.emails?.[0]?.value,
+          );
+          done(null, user);
+        },
+      ),
+    );
+  }
+}
+
+export default PassportService;
