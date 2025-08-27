aed65419ca53894780c5170c9e505833aa0590d0
Alex Smith <rdutta23@yahoo.com>
2025-08-26 23:14:31 -0700

    google flows

---
+++ src/service-auth/auth-controller-google.ts
@@ -0,0 +1,26 @@
+import { Controller, Get, Req, Res, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import type { Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
+import type { UserAccountGetResponse } from '../dto/user-account-dto/user-account-get-response';
+
+interface RequestWithUser extends Request {
+  user: UserAccountGetResponse;
+}
+
+@Controller('auth')
+export class AuthController {
+  @Get('google')
+  @UseGuards(AuthGuard('google'))
+  async googleAuth() {}
+
+  @Get('google/callback')
+  @UseGuards(AuthGuard('google'))
+  async googleAuthCallback(@Req() req: RequestWithUser, @Res() res: Response) {
+    const token = jwt.sign(
+      { id: req.user.user_account_id },
+      process.env.JWT_SECRET!,
+    );
+    res.json({ user: req.user, token });
+  }
+}
